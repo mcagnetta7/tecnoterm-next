851d1f4940a0c2556ba0b9bcfa86e89d1d10600e
Matti Cagnetta <matti@tecnoterm.it>
2026-06-26 17:04:46 +0200

    no message

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   // ⚠️ CAMBIA QUESTO A true PER ATTIVARE MANUTENZIONE
-  const maintenanceMode = true;
+  const maintenanceMode = false;
 
   if (maintenanceMode) {
     return NextResponse.redirect(new URL('/maintenance', request.url));
